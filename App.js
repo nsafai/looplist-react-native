99c8071304a097d83b0519e6659c0b36330dbc42
Nicolai Safai <nicolaisafai@gmail.com>
2019-04-25 14:32:54 -0700

    mvp of tabs with icons complete

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { Ionicons } from 'react-native-vector-icons'
+
 
 class HomeScreen extends React.Component {
   render() {
+
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={styles.home}>
         <Text>Home!</Text>
+        <Ionicons name="ios-home" size={32} />
+        {/* <Ionicons.Button name='ios-add-circle-outline' size={32} /> */}
       </View>
     );
   }
@@ -14,26 +19,51 @@ class HomeScreen extends React.Component {
 
 class SettingsScreen extends React.Component {
   render() {
+
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={styles.settings}>
         <Text>Settings!</Text>
+        <Ionicons name="ios-settings" size={32} />
       </View>
     );
   }
 }
 
-const TabNavigator = createBottomTabNavigator({
-  Home: HomeScreen,
-  Settings: SettingsScreen,
-});
+// createBottomTabNavigator(RouteConfigs, BottomTabNavigatorConfig)
+const TabNavigator = createBottomTabNavigator(
+  { // Route Configs
+    Home: HomeScreen,
+    Settings: SettingsScreen,
+  }, 
+  { // BottomTabNavigatorConfig
+    defaultNavigationOptions: ({ navigation}) => ({
+        tabBarIcon: ({ focused, horizontal, tintColor }) => {
+            const { routeName } = navigation.state;
+            let iconName;
+            switch(routeName) {
+              case 'Home': 
+                iconName = 'ios-home'
+                break
+              case 'Settings': 
+                iconName = 'ios-settings'
+            }
+            return <Ionicons name={iconName} size={25} color={tintColor} />
+        }
+    })
+  }
+);
 
 export default createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
-  container: {
+  home: { 
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
+  },
+  settings: { 
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 });
